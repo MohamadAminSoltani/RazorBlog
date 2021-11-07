309c742bf45319d5c8a8dcde9bb66f001411a04d
M.A.Soltani <maxmadmas@gmail.com>
2021-11-07 13:45:04 +0330

    Add project files.

---
+++ RazorBlog/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorBlog.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorBlog
+{
+    public class BlogContext:DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options):base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMapping());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
